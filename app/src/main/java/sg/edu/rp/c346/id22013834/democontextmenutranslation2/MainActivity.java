19ebebcdebdde93d6d90f3f39c9c29abac7da56d
av4lyfo <22013834@myrp.edu.sg>
2023-05-30 14:40:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22013834/democontextmenutranslation2/MainActivity.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.id22013834.democontextmenutranslation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvTranslatedText = findViewById(R.id.TextView);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+}
